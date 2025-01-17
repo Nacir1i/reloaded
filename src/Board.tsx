8a145ac67046caa6b4f2cb69a6504b6382b45218
PrOeZ <idouaddi.ayoub@gmail.com>
2025-01-17 12:54:43 +0100

    fixed x & y axis + code refactor

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,67 +1,79 @@
-import { useEffect, useState } from "react";
-import { chessPiece } from "./App";
+import { useState } from "react";
+import { ChessPiece } from "./App";
+
+const getColumnLetter = (col: number): string => {
+  return String.fromCharCode(96 + col);
+};
 
 type CellProps = {
-    index: number;
-    position: {
-        x: number;
-        y: number;
-    };
-    chessPiece: chessPiece | undefined;
-}
+  index: number;
+  position: {
+    x: number;
+    y: number;
+  };
+  ChessPiece: ChessPiece | undefined;
+};
 
 function Cell(props: CellProps) {
-    return (
-        <div
-            className={`flex relative items-center justify-center ${props.index % 2 ? 'bg-slate-300' : 'bg-slate-700'}`}
-        >
-            <p className="absolute top-0 left-0 text-xs text-red-600">{ props.position.x }, { props.position.y }</p>
-            <img className=" w-2/3" src={props.chessPiece?.image} alt="" />
-        </div>
-    )
+  const isFirstColumn = props.position.y === 1;
+  const isLastRow = props.position.x === 1;
+  // fixed white/black colors
+  return (
+    <div
+      className={`flex relative items-center justify-center ${
+        props.index % 2 ? "bg-slate-700" : "bg-slate-300"
+      }`}
+    >
+      <p className="absolute top-1 left-2 text-xl text-red-600">
+        {isFirstColumn && props.position.x}
+      </p>
+      <p className="absolute bottom-1 right-2 text-2xl text-red-600">
+        {isLastRow && getColumnLetter(props.position.y)}
+      </p>
+      <img className=" w-2/3" src={props.ChessPiece?.image} alt="" />
+    </div>
+  );
 }
 
 type BoardProps = {
-    pieces: chessPiece[];
-}
+  pieces: ChessPiece[];
+};
 
 function Board(props: BoardProps) {
-    const ROWS = 8;
-    const COLS = 8;
+  const ROWS = 8;
+  const COLS = 8;
 
-    const [cells] = useState<CellProps[]>(() => {
-        const cellsArray: CellProps[] = [];
-        
-        for (let rowIndex = 1; rowIndex <= ROWS; rowIndex++) {
-            for (let colIndex = 1; colIndex <= COLS; colIndex++) {
-                cellsArray.push({
-                    index: rowIndex % 2 ? colIndex : colIndex + 1,
-                    position: { x: rowIndex, y: colIndex },
-                    chessPiece: props.pieces.find(piece => piece.position.x === rowIndex && piece.position.y === colIndex),
-                });
+  const [cells] = useState<CellProps[]>(() => {
+    const cellsArray: CellProps[] = [];
+    // fixed the x & y axis
+    for (let rowIndex = ROWS; rowIndex >= 1; rowIndex--) {
+      for (let colIndex = 1; colIndex <= COLS; colIndex++) {
+        cellsArray.push({
+          index: colIndex % 2 ? rowIndex : rowIndex + 1,
+          position: { x: rowIndex, y: colIndex },
+          ChessPiece: props.pieces.find(
+            (piece) =>
+              piece.position.x === rowIndex && piece.position.y === colIndex
+          ),
+        });
+      }
+    }
 
-            }
-        }
-        
-        return cellsArray;
-    });
+    return cellsArray;
+  });
 
-    useEffect(() => {
-        console.log(cells);
-    }, [cells]);
-
-    return (
-        <div className="w-full max-w-[800px] h-[800px] grid grid-cols-8 grid-rows-8">
-            {cells.map((cell, index) => (
-                <Cell 
-                    key={index} 
-                    index={cell.index} 
-                    position={cell.position}
-                    chessPiece={cell.chessPiece}
-                />
-            ))}
-        </div>
-    );
+  return (
+    <div className="w-full max-w-[800px] h-[800px] grid grid-cols-8 grid-rows-8">
+      {cells.map((cell, index) => (
+        <Cell
+          key={index}
+          index={cell.index}
+          position={cell.position}
+          ChessPiece={cell.ChessPiece}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Board;
